e37856d4fdbf6a5759af3a006a34e95d2f1bd10d
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-06-02 22:45:07 +0700

    List in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
-const welcome = {
-    greeting: 'Hey',
-    title: 'React',
-};
-
-function getTitle(title) {
-    return title;
-}
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectId: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectId: 1,
+    },
+];
 
 function App() {
 
     return (
         <div>
-            <h1>{welcome.greeting} {welcome.title}</h1>
-            <h1>Hello {getTitle('React')}</h1>
+            <h1>My hacker stories</h1>
 
             <label htmlFor="search">Search</label>
             <input id="search" type="text"/>
+            <hr/>
+
+            {
+                // list.map(item => {
+                //     return <div>{item.title}</div>
+                // })
+
+                // list.map(item => (<div>{item.title}</div>))
+
+                list.map(item => {
+                    return (
+                        <div key={item.objectId}>
+                            {/*{item.title}*/}
+                            <span>
+                                <a href={item.url}>{item.title}</a>
+                            </span>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </div>
+                    );
+                })
+            }
+
+
+
         </div>
     );
 }
