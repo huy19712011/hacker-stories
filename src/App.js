0811849d4f43a4955909c1640a94f239c7328546
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-06-03 16:18:50 +0700

    Callback handlers in JSX

--- src/App.js
+++ src/App.js
@@ -24,24 +24,18 @@ const App = () => {
         },
     ];
 
-    const [searchTerm, setSearchTerm] = React.useState('');
+    const handleSearch = event => {
 
-    // do something here
-    const handleChange = event => {
-
-        setSearchTerm(event.target.value);
+        console.log(event.target.value);
     };
+
 
     return (
         <div>
             <h1>My hacker stories</h1>
 
-            <label htmlFor="search">Search</label>
-            <input id="search" type="text" onChange={handleChange}/>
+            <Search onSearch={handleSearch}></Search>
 
-            <p>
-                Searching for <strong>{searchTerm}</strong>
-            </p>
             <hr/>
 
             <List list={stories}/>
@@ -49,6 +43,29 @@ const App = () => {
     );
 };
 
+const Search = (props) => {
+
+    const [searchTerm, setSearchTerm] = React.useState('');
+
+    const handleChange = event => {
+
+        setSearchTerm(event.target.value);
+
+        props.onSearch(event);
+    };
+
+    return (
+        <div>
+            <label htmlFor="search">Search</label>
+            <input id="search" type="text" onChange={handleChange}/>
+
+            <p>
+                Searching for <strong>{searchTerm}</strong>
+            </p>
+        </div>
+    );
+
+};
 
 const List = props =>
     props.list.map(item => (
@@ -63,3 +80,13 @@ const List = props =>
     ));
 
 export default App;
+
+/*
+Consider the concept of the callback handler: We pass a function from one component (App) to
+another component (Search); we call it in the second component (Search); but have the actual
+implementation of the function call in the first component (App). This way, we can communicate up
+the component tree. A handler function used in one component becomes a callback handler, which
+is passed down to components via React props. React props are always passed down as information
+the component tree, and callback handlers passed as functions in props can be used to communicate
+up the component hierarchy.
+*/
