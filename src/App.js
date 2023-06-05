20377d7494312bd68bf377b39269ee925f54a18b
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-06-05 19:28:43 +0700

    3rd party libraries in react: asios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import './App.css';
 
@@ -76,12 +77,12 @@ const App = () => {
 
         dispatchStories({type: 'STORIES_FETCH_INIT'});
 
-        fetch(url)
-            .then(response => response.json())
+        axios
+            .get(url)
             .then(result => {
                 dispatchStories({
                     type: 'STORIES_FETCH_SUCCESS',
-                    payload: result["hits"],
+                    payload: result.data["hits"],
                 });
             })
             .catch(() => dispatchStories({type: 'STORIES_FETCH_FAILURE'}));
